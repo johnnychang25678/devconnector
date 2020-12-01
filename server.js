df83174976fdd14a40af1b75cb325bdc9ca727e2
Johnny Chang <zjps3407@gmail.com>
2020-12-01 10:11:18 +0800

    add and delete experience and education

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('API Running'))
+// connect database
+connectDB()
+
+// init middleware to access req.body
+app.use(express.json({ extended: false }))
+
+app.get('/', (req, res) => {
+  // console.log(req)
+  res.send('API running')
+})
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/post', require('./routes/api/post'))
 
 const PORT = process.env.PORT || 5000
 
