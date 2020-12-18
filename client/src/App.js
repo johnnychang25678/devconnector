ae15b4f28e5a67d1285f44b8eabc444d72a50339
Johnny Chang <zjps3407@gmail.com>
2020-12-18 14:29:13 +0800

    add create profile function

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import Alert from './components/layout/Alert'
 import Dashboard from './components/dashboard/Dashboard'
+import CreateProfile from './components/profile-forms/CreateProfile'
+
 import PrivateRoute from './components/routing/PrivateRoute'
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
@@ -38,6 +40,7 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/create-profile" component={CreateProfile} />
             </Switch>
           </section>
         </React.Fragment>
