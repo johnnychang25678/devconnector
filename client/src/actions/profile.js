d5aa68631167176c457820ec4a52ef46d88447d5
Johnny Chang <zjps3407@gmail.com>
2021-02-18 23:42:35 +0800

    add profile actions for dashboard display

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { setAlert } from './alert'
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, ACCOUNT_DELETED, CLEAR_PROFILE } from './types'
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, ACCOUNT_DELETED, CLEAR_PROFILE, GET_PROFILES, GET_REPOS } from './types'
 
 // Get current users profile
 export const getCurrentProfile = () => async dispatch => {
@@ -18,6 +18,60 @@ export const getCurrentProfile = () => async dispatch => {
     })
   }
 }
+
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE }) // prevent flashing of past user's profile
+  try {
+    const res = await axios.get('/api/profile')
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
+  }
+}
+
+// Get profile by id
+export const getProfileById = userId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`)
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
+  }
+}
+
+// Get Github repos
+export const getGithubRepos = username => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE }) // prevent flashing of past user's profile
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`)
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
+  }
+}
+
 
 // Create or update profile
 export const createProfile = (formData, history, edit = false) => async dispatch => {
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { CLEAR_PROFILE, GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "../actions/types"
+import { CLEAR_PROFILE, GET_PROFILE, GET_PROFILES, GET_REPOS, PROFILE_ERROR, UPDATE_PROFILE } from "../actions/types"
 
 const initialState = {
   profile: null,
@@ -18,6 +18,12 @@ export default function profile(state = initialState, action) {
         profile: payload,
         loading: false
       }
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      }
     case PROFILE_ERROR:
       return {
         ...state,
@@ -29,6 +35,12 @@ export default function profile(state = initialState, action) {
         ...state,
         profile: null,
         repos: [],
+        loading: false
+      }
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false
       }
     default:
