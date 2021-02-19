6d839c4b7fd55f72fbe7ee3810c888af7f8a49c1
Johnny Chang <zjps3407@gmail.com>
2021-02-19 23:35:50 +0800

    add addComment feature

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem'
+import CommentForm from './CommentForm'
 import { getPost } from '../../actions/post';
 
 
@@ -16,6 +17,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       <Fragment>
         <Link to="/posts" className="btn">Back to post</Link>
         <PostItem post={post} showActions={false} />
+        <CommentForm postId={post._id} />
       </Fragment>
     )
   )
